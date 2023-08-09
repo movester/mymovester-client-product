e594b3882d8630a6ec1d422b6a5903bd59b06636
carina <hj_kim1@caredoc.kr>
2023-08-09 17:24:40 +0900

    fe-11 | feature: stretching 검색 결과 화면 구현

--- pages/stretching/index.tsx
+++ pages/stretching/index.tsx
@@ -8,6 +8,7 @@ import ComboBox from "../../components/basic/ComboBox";
 import { useState } from "react";
 import Input from "../../components/basic/Input";
 import { useRouter } from "next/router";
+import Box from "../../components/basic/Box";
 
 const mainCatergory = [
   { name: "전체", id: "total" },
@@ -39,10 +40,16 @@ const effectsCategory = [
   { name: "라운드숄더 완화", id: "e6" },
 ];
 
+const listType = [
+  { name: "최신순", id: "latest" },
+  { name: "인기순", id: "famous" },
+];
+
 const StrechingPage = () => {
   const [mainValue, setMainValue] = useState(undefined);
   const [subValue, setSubValue] = useState(undefined);
   const [effectValue, setEffectValue] = useState(undefined);
+  const [listTypeValue, setListTypeValue] = useState(listType[0]);
   const [inputValue, setInputValue] = useState<string>("");
 
   const router = useRouter();
@@ -63,13 +70,19 @@ const StrechingPage = () => {
             </Button>
           </BreadCrumb>
         </ShadowBox>
-        <SearchBox>
-          <ComboBoxBox>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="start"
+          width="100%"
+        >
+          <Box display="flex" justifyContent="start" alignItems="start" gap={8}>
             <ComboBox
               size="sm"
               list={mainCatergory}
               value={mainValue}
               setValue={setMainValue}
+              label="대분류"
             ></ComboBox>
             <ComboBox
               size="sm"
@@ -81,6 +94,7 @@ const StrechingPage = () => {
               }
               value={subValue}
               setValue={setSubValue}
+              label="중분류"
             ></ComboBox>
             <ComboBox
               size="sm"
@@ -88,16 +102,125 @@ const StrechingPage = () => {
               list={effectsCategory}
               value={effectValue}
               setValue={setEffectValue}
+              label="효과"
             ></ComboBox>
-          </ComboBoxBox>
-          <Box>
+          </Box>
+          <Box width={400}>
             <Input
               placeholder="제목으로 검색"
               value={inputValue}
               setValue={setInputValue}
             ></Input>
           </Box>
-        </SearchBox>
+        </Box>
+        <ShadowBox>
+          <Box
+            padding={32}
+            overflow="scroll"
+            gap={32}
+            display="flex"
+            flexDirection="column"
+          >
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="end"
+              width={"100%"}
+            >
+              <Typography variants="body2" color={colors.g100}>
+                총 30개
+              </Typography>
+              <ComboBox
+                size="xs"
+                value={listTypeValue}
+                setValue={setListTypeValue}
+                list={listType}
+              ></ComboBox>
+            </Box>
+            <Table>
+              <TableGrid>
+                <TableItem>
+                  <Typography variants="body1" color={colors.g300}>
+                    제목
+                  </Typography>
+                </TableItem>
+                <TableItem>
+                  <Typography variants="body1" color={colors.g300}>
+                    부위
+                  </Typography>
+                </TableItem>
+                <TableItem>
+                  <Typography variants="body1" color={colors.g300}>
+                    효과
+                  </Typography>
+                </TableItem>
+                <TableItem>
+                  <Typography variants="body1" color={colors.g300}>
+                    조회수
+                  </Typography>
+                </TableItem>
+                <TableItem>
+                  <Typography variants="body1" color={colors.g300}>
+                    등록일시
+                  </Typography>
+                </TableItem>
+              </TableGrid>
+              <TableGirdWrapper>
+                <TableGrid>
+                  <TableItem>
+                    <Typography variants="body1">거북목 스트레칭</Typography>
+                  </TableItem>
+                  <TableItem>
+                    <Typography variants="body1">{"상체 > 등/몸통"}</Typography>
+                  </TableItem>
+                  <TableItem>
+                    <Typography variants="body1">자세개선, 통증완화</Typography>
+                  </TableItem>
+                  <TableItem>
+                    <Typography variants="body1">30</Typography>
+                  </TableItem>
+                  <TableItem>
+                    <Typography variants="body1">2023-08-01</Typography>
+                  </TableItem>
+                </TableGrid>
+                <TableGrid>
+                  <TableItem>
+                    <Typography variants="body1">거북목 스트레칭</Typography>
+                  </TableItem>
+                  <TableItem>
+                    <Typography variants="body1">{"상체 > 등/몸통"}</Typography>
+                  </TableItem>
+                  <TableItem>
+                    <Typography variants="body1">자세개선, 통증완화</Typography>
+                  </TableItem>
+                  <TableItem>
+                    <Typography variants="body1">30</Typography>
+                  </TableItem>
+                  <TableItem>
+                    <Typography variants="body1">2023-08-01</Typography>
+                  </TableItem>
+                </TableGrid>
+                <TableGrid>
+                  <TableItem>
+                    <Typography variants="body1">거북목 스트레칭</Typography>
+                  </TableItem>
+                  <TableItem>
+                    <Typography variants="body1">{"상체 > 등/몸통"}</Typography>
+                  </TableItem>
+                  <TableItem>
+                    <Typography variants="body1">자세개선, 통증완화</Typography>
+                  </TableItem>
+                  <TableItem>
+                    <Typography variants="body1">30</Typography>
+                  </TableItem>
+                  <TableItem>
+                    <Typography variants="body1">2023-08-01</Typography>
+                  </TableItem>
+                </TableGrid>
+              </TableGirdWrapper>
+            </Table>
+          </Box>
+        </ShadowBox>
       </ContentWrapper>
     </PageWrapper>
   );
@@ -125,21 +248,35 @@ const BreadCrumb = styled.div`
   padding: 16px 16px 16px 32px;
 `;
 
-const SearchBox = styled.div`
+const Table = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: start;
+  flex-direction: column;
+  background-color: ${colors.g300};
+  gap: 1px;
+`;
+
+const TableGirdWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.f200};
+  gap: 1px;
+`;
+
+const TableGrid = styled.div`
+  background-color: ${colors.f200};
+  gap: 1px;
+  display: grid;
+  grid-template-columns:
+    minmax(400px, 10fr) minmax(160px, 4fr) minmax(240px, 6fr)
+    minmax(80px, 2fr) minmax(120px, 3fr);
+  align-items: end;
+`;
+const TableItem = styled.div`
+  background-color: ${colors.f000};
   width: 100%;
-`;
-
-const ComboBoxBox = styled.div`
+  padding: 16px;
+  height: 100%;
+  text-align: start;
   display: flex;
-  justify-content: start;
-  align-items: start;
-  gap: 8px;
-`;
-
-const Box = styled.div`
-  width: 400px;
-  padding: 0;
+  align-items: center;
 `;
