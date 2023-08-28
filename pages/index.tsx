467001554dba0b91c4640b40e72866657a9d14ca
carina <hj_kim1@caredoc.kr>
2023-08-28 11:28:57 +0900

    styled-component 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 
+import styled from "styled-components";
+
 const mainPage = () => {
   return (
     <>
-      <div>work?</div>
+      <Div>work?</Div>
     </>
   );
 };
 
 export default mainPage;
+
+const Div = styled.div`
+  background-color: red;
+  color: blue;
+  font-size: 40px;
+`;
