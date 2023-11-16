ee4efaf60abf43901480059ea7f117a8f415a193
chloe41297 <grace97041292@gmail.com>
2023-11-16 16:11:00 +0900

    fe-12 | feature : 모달 생성

--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -87,7 +87,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "67e8dd9797968f77"; }
+/******/ 	__webpack_require__.h = function() { return "4b25f20a88c9f4d3"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
--- components/utils/Navigator.tsx
+++ components/utils/Navigator.tsx
@@ -3,7 +3,9 @@ import { colors } from "../../constants/style";
 import { useRouter } from "next/router";
 import useIsMobile from "../../hooks/utils/useIsMobile";
 import Image from "next/image";
-import { Box } from "movester-design-system";
+import { Box, Typography } from "movester-design-system";
+import { useRef, useState } from "react";
+import Modal from "./Modal";
 
 interface IStyledProps {
   ismobile: boolean;
@@ -12,22 +14,76 @@ interface IStyledProps {
 const Navigator = () => {
   const router = useRouter();
   const isMobile = useIsMobile();
+  const [isLoggined, setIsLoggined] = useState(true);
+  const [isModalOpened, setIsModalOpened] = useState(false);
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const modalOutSideClick = (e: any) => {
+    if (modalRef.current === e.target) {
+      setIsModalOpened(false);
+    }
+  };
   return (
-    <Wrapper ismobile={isMobile}>
-      <Box
-        flexDirection="column"
-        justifyContent="start"
-        alignItems="center"
-        onClick={() => router.push("/stretchings")}
-      >
-        <Image
-          src={isMobile ? "/favicon.ico" : "/logo.png"}
-          width={isMobile ? 16 : 120}
-          height={isMobile ? 16 : 32}
-          alt={""}
-        ></Image>
-      </Box>
-    </Wrapper>
+    <>
+      {isModalOpened && (
+        <Modal modalRef={modalRef} modalOutsideClick={modalOutSideClick}>
+          <MyPageModal>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+              justifyContent="center"
+              padding={8}
+            >
+              <Typography variants="body3">movcoco님</Typography>
+            </Box>
+            <MyPageModalListItem>
+              <Typography variants="body3">마이페이지</Typography>
+            </MyPageModalListItem>
+            <MyPageModalListItem>
+              <Typography variants="body3">로그아웃</Typography>
+            </MyPageModalListItem>
+          </MyPageModal>
+        </Modal>
+      )}
+      <Wrapper ismobile={isMobile}>
+        <Box
+          flexDirection="row"
+          justifyContent="start"
+          alignItems="center"
+          onClick={() => router.push("/stretchings")}
+        >
+          <Image
+            src={isMobile ? "/favicon.ico" : "/logo.png"}
+            width={isMobile ? 16 : 120}
+            height={isMobile ? 16 : 32}
+            alt={""}
+          ></Image>
+        </Box>
+        <AccountWrapper>
+          {isLoggined ? (
+            <>
+              <Box
+                width={30}
+                height={30}
+                borderRadius={15}
+                backgroundColor={colors.g000}
+                border={
+                  isModalOpened
+                    ? `1px solid ${colors.vividPrimaryColor}`
+                    : "none"
+                }
+                onClick={() => setIsModalOpened((prev) => !prev)}
+              ></Box>
+            </>
+          ) : (
+            <Box>
+              <Typography variants="body3">로그인 | 회원가입</Typography>
+            </Box>
+          )}
+        </AccountWrapper>
+      </Wrapper>
+    </>
   );
 };
 
@@ -42,7 +98,45 @@ const Wrapper = styled.div<IStyledProps>`
   top: 0;
   box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.1);
   z-index: 300;
+  display: flex;
+  justify-content: space-between;
   :hover {
+    cursor: pointer;
+  }
+`;
+const AccountWrapper = styled.div`
+  /* position: relative; */
+  padding-right: 32px;
+`;
+const FullWrapper = styled.div``;
+const MyPageModal = styled.div`
+  position: absolute;
+  right: 32px;
+  top: 48px;
+  background-color: ${colors.f000};
+  border: 1px solid ${colors.g200};
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  width: 180px;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 8px;
+  gap: 4px;
+`;
+
+const MyPageModalListItem = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  padding: 8px;
+  border-radius: 4px;
+  outline: none;
+  border: 0;
+  background-color: transparent;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
     cursor: pointer;
   }
 `;
