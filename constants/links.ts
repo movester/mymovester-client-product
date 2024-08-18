85add9d7e17869b21b9d35adffc230e5284d5843
chloe41297 <grace97041292@gmail.com>
2024-08-18 17:16:24 +0900

    fe-31 : .next gitignore 적용

--- constants/links.ts
+++
@@ -1,7 +0,0 @@
-export const LINK_KAKAO_INQUIRY = "http://pf.kakao.com/_NclkK/chat";
-export const LINK_USAGE_POLICY =
-  "https://movester.notion.site/1-0-13e2e2b4e62f4683b2e5013f2337f8f2?pvs=4";
-export const LINK_PRIVACY_POLICY =
-  "https://movester.notion.site/1-0-bfbb290d7b54428c8ed13137ae15a3d8?pvs=4";
-export const LINK_MARKETING_POLICY =
-  "https://movester.notion.site/1-0-09b57384fa7b46909280f7d30b74e17c?pvs=4";
--- hooks/utils/useLoggined.ts
+++
@@ -1,5 +0,0 @@
-const useLoggined = (): boolean => {
-  return JSON.parse(window.localStorage.getItem("_uToken")) ? true : false;
-};
-
-export default useLoggined;
